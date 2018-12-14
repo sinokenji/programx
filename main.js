ed6385ba3cef1e7094085c4deca6e8f5fb86639c
XXU34 <XXU34@5CG7412N32.KONENET.COM>
2018-12-14 17:18:34 +0800

    First Commit

---
+++ main.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.send('HEY! How are you doing? ')
+})
+app.listen(3000, () => console.log('Server running on port 3000'))
+
+
+
